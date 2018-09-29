eaa98e538a402aa8cf348993a3272621decb7ff7
Miguelslo27 <miguel.sosa@globant.com>
2018-09-29 18:58:12 -0300

    Comments cleaned

--- lib/main.dart
+++ lib/main.dart
@@ -18,32 +18,6 @@ class MyScaffold extends StatelessWidget {
     return Material(
       child: Column(
         children: <Widget> [
-          // Refactorizar
-          // Container(
-          //   height: 56.0,
-          //   padding: const EdgeInsets.symmetric(horizontal: 8.0),
-          //   decoration: BoxDecoration(color: Colors.blue[500]),
-          //   child: Row(
-          //     children: <Widget> [
-          //       IconButton(
-          //         icon: Icon(Icons.menu),
-          //         tooltip: 'Navigation menu',
-          //         onPressed: null // null disables the button
-          //       ),
-          //       Expanded(
-          //         child: Text(
-          //           "Example title",
-          //           style: Theme.of(context).primaryTextTheme.title
-          //         )
-          //       ),
-          //       IconButton(
-          //         icon: Icon(Icons.search),
-          //         tooltip: 'Search',
-          //         onPressed: null
-          //       )
-          //     ]
-          //   ),
-          // ),
           MyAppBar(
             title: "Title for navbar"
           ),
