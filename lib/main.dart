df5abaeff972d0f47a5e01fcaf88c78e9a8cfd24
Miguelslo27 <miguel.sosa@globant.com>
2018-09-29 18:47:21 -0300

    Added a complete example screen with Navbar and content

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,50 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    Center(
-      child: Text(
-        "Hello World!",
-        textDirection: TextDirection.ltr
+  runApp(MaterialApp(
+    title: "Flutter Widget Framework",
+    home: MyScaffold()
+  ));
+}
+
+class MyScaffold extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Column(
+        children: <Widget> [
+          Container(
+            height: 56.0,
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            decoration: BoxDecoration(color: Colors.blue[500]),
+            child: Row(
+              children: <Widget> [
+                IconButton(
+                  icon: Icon(Icons.menu),
+                  tooltip: 'Navigation menu',
+                  onPressed: null // null disables the button
+                ),
+                Expanded(
+                  child: Text(
+                    "Example title",
+                    style: Theme.of(context).primaryTextTheme.title
+                  )
+                ),
+                IconButton(
+                  icon: Icon(Icons.search),
+                  tooltip: 'Search',
+                  onPressed: null
+                )
+              ]
+            ),
+          ),
+          Expanded(
+            child: Center(
+              child: Text("Hello World!")
+            )
+          )
+        ]
       )
-    )
-  );
+    );
+  }
 }
